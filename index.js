6e41a4af3ed0ab71564275cede3d4c24645353e0
agusslam <agus.slamethermawan@gmail.con>
2021-10-10 21:27:10 +0700

    berhasil membuat response json

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const http = require("http")
 const fs = require("fs")
 
+// function onRequest(request, response){
+//     response.writeHead(200, { "Content-Type": "text/html" })
+//     fs.readFile("index.html", null, (error, data) => {
+//         if (error) {
+//             response.writeHead(404)
+//             response.write("file not found")
+//         } else {
+//             response.write(data)
+//         } response.end()
+//     })
+// }
+
+//response json
 function onRequest(request, response){
-    response.writeHead(200, { "Content-Type": "text/html" })
-    fs.readFile("index.html", null, (error, data) => {
-        if (error) {
-            response.writeHead(404)
-            response.write("file not found")
-        } else {
-            response.write(data)
-        } response.end()
-    })
+    response.writeHead(200, { "Content-Type" : "application/json"})
+    const data = {
+        name : "Yudi Kris",
+        age : 22
+    }
+    response.end(JSON.stringify(data))
 }
 
 http.createServer(onRequest).listen(8000)
